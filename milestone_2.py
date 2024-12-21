105c34b48a092eca4c27ef9c0d2b0b220c11d5b1
Auwal Musa <153789969+auwalmusa@users.noreply.github.com>
2024-12-21 14:54:05 +0000

    Add milestone_2.py and user_input

--- milestone_2.py
+++ milestone_2.py
@@ -1,3 +1,5 @@
+import random
+
 #Step 1:
 #Creating a list containing the names of 5 favorite fruits.
 my_favourite_fruits = ["apple", "pears", "watermelon", "orange", "banana"]
@@ -7,3 +9,19 @@ word_list = my_favourite_fruits
 
 #step 3; printing out the created list(word_list)
 print(word_list)
+
+#step 4: selecting a random word from the list using randon.choice() function
+word = random.choice(word_list)
+
+#step 5: printing out the selected random word
+print(word)
+
+#task 3
+guess = input("Enter a single letter: ")
+print("Your guesses letter:", guess)
+
+#task 4
+if len(guess) == 1 and guess.isalpha():
+    print("Good Guess")
+else:
+    print("Oops! That is not a valid input")
