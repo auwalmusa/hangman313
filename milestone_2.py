84cf15d20be305a2791b2fde2f348ae00be885c9
Auwal Musa <153789969+auwalmusa@users.noreply.github.com>
2024-12-21 14:43:46 +0000

    Add Hangman milestone_2.py and iniial README

---
+++ milestone_2.py
@@ -0,0 +1,9 @@
+#Step 1:
+#Creating a list containing the names of 5 favorite fruits.
+my_favourite_fruits = ["apple", "pears", "watermelon", "orange", "banana"]
+
+# step assingin he list of my favourite fruite to variable word_list
+word_list = my_favourite_fruits
+
+#step 3; printing out the created list(word_list)
+print(word_list)
